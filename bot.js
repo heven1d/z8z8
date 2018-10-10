b9ef57147b1cc292eb3eb69e62818724d5941fe0
rakanaltahhan <44034112+rakanaltahhan@users.noreply.github.com>
2018-10-10 21:52:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -86,46 +86,6 @@ client.on('message', message => {
     }
 });
 
-
-  client.on('message', message => {
-    if (message.content.split(' ')[0] == 'wbc')
-       message.guild.members.forEach( member => {
-         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-
-
-           member.send( ` ${member} ` + "**" + "  ** " + message.content.substr(3));
-                                                      message.delete();
-            
-                                                    });
-            
-                                                  });
-   client.on("message", message => {
-       var prefix = "!";
- 
-             var args = message.content.substring(prefix.length).split(" ");
-                if (message.content.startsWith(prefix + "b")) {
-                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-
-                          if (!args[1]) {
-                            
-                                 let embed3 = new Discord.RichEmbed()
-                                     .setDescription(":white_check_mark: | تم ارسال رسالة لا يوجد فيها شيء")
-                                       .setColor("#FF00FF")
-                                          message.channel.sendEmbed(embed3);
-                            
-                                        } else {
-
-                            
-                                           let embed4 = new Discord.RichEmbed()
-                                                            .setDescription(':white_check_mark: | تم ارسال الرساله للجميع ..')
-                                                                .setColor("#99999")
-                               
-                                                                message.channel.sendEmbed(embed4);
-                                                      message.delete();
-                            }
-                          }
-});
-
 client.on('message', message => {
      if (message.author.bot) return;
     if (message.content.startsWith("رابط")) {
