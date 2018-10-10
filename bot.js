ad58844869a26df480ca2a417943ba7af47377a1
rakanaltahhan <44034112+rakanaltahhan@users.noreply.github.com>
2018-10-10 20:44:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,4 +27,13 @@ client.user.setGame(` Rabbit Community.  `,"http://twitch.tv/S-F")
   console.log('')
 });
 
+client.on('message',function(message) {
+    let prefix = "!";
+let args = message.content.split(" ").slice(1).join(" ");
+if(message.content.startsWith(prefix + "say")) {
+if(!args) return;
+message.channel.send(`**# ${args}**`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
+}
+});
+
 client.login(process.env.BOT_TOKEN);
