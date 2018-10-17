1edc3c57c819631e3552784572336d1cbd6a772b
rakanaltahhan <44034112+rakanaltahhan@users.noreply.github.com>
2018-10-17 21:09:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -128,23 +128,34 @@ client.on('message', function(message) {
 
 client.on('message', message => {
     if (message.content.startsWith("رابط")) {
-
+ 
   message.channel.createInvite({
         thing: true,
-        maxUses: 5,
+        maxUses: 100,
         maxAge: 86400
     }).then(invite =>
       message.author.sendMessage(invite.url)
     )
-  message.channel.send("**تم ارسال الرابط برسالة خاصة**")
-
-message.author.send(`**       مدة الرابط : يـوم        
-عدد استخدامات الرابط : 5     **`)
-
-
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setDescription("| :white_check_mark:   تم ارسال الرابط على الخاص  ")
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+                .setAuthor(message.guild.name, message.guild.iconURL)
+        .setDescription(`
+**
+---------------------
+-[${message.guild.name}]  هذا هو رابط سيرفر
+---------------------
+-هذا الرابط صالح ل 100 مستخدم فقط
+---------------------
+-هذا الرابط صالح لمده 24 ساعه فقط
+---------------------
+**`)
+      message.author.sendEmbed(Embed11)
     }
 });
-
 
 
 client.on('message', message => {
