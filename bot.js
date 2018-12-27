0eeae2d67f0ad4bb899b813d86121dc02e76bb4e
rakanaltahhan <44034112+rakanaltahhan@users.noreply.github.com>
2018-12-27 23:00:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -71,25 +71,6 @@ message.author.sendEmbed(embed)
 
 
 
-client.on('message', message => {
-    if (message.content.startsWith("^stats")) {
-    message.channel.send({
-        embed: new Discord.RichEmbed()
-            .setColor('RANDOM')
-            .setTitle('Stats Bot / Info ')
-            .addField('``Uptime``', timeCon(process.uptime()), true)
-            .addField('``Ping Is``' , `${Date.now() - message.createdTimestamp}` + '``Ms``', true)
-            .addField('``RAM Usage``', `${(process.memoryUsage().rss / 1048576).toFixed()}MB`, true)
-            .addField('``Guild Count``', client.guilds.size, true)
-            .addField('``Bot In channel``' , `${client.channels.size}` , true)
-            .addField('``Users rout``' ,`${client.users.size}` , true)
-            .addField('``Name Bot Or tag``' , `${client.user.tag}` , true)
-            .addField('``Bot Id``' , `${client.user.id}` , true)
-            .setFooter('! ts Team')
-    })
-});
-
-
 
 
 client.on('message', message => {
