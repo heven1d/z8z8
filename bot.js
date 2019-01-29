e406f515e95167c477f1ba30c1ad6912ef1f1362
Netflixl <47156778+Netflixl@users.noreply.github.com>
2019-01-30 00:28:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -260,7 +260,7 @@ client.on('message' , message => {
 
 client.on('message',async msg => {//Toxic Codes
   var p = "-";//Toxic Codes
-  if(msg.content.startsWith(p + "setuser")) {//Toxic Codes
+  if(msg.content.startsWith("^setuser")) {//Toxic Codes
   if(!msg.guild.member(msg.author).hasPermissions('MANAGE_CHANNELS')) return msg.reply('❌ **ليس لديك صلاحيه**');//Toxic Codes
   if(!msg.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS'])) return msg.reply('❌ **البوت لا يمتلك صلاحية**');//Toxic Codes
   msg.guild.createChannel(`Users : → ${client.users.size} ←` , 'voice').then(time => {//Toxic Codes
