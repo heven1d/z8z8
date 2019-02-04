beff9d9b6d6fa3c2dffbfc3c72466dfb6c62ee8f
Netflixl <47156778+Netflixl@users.noreply.github.com>
2019-02-04 22:22:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const devs = ["460606140666085378" , "284005623895425024"];
+const devs = ["460606140666085378"];
+const developers = ["460606140666085378"]
 const adminprefix = ["-"];
 var prefix = "^";  
 
@@ -32,30 +33,42 @@ client.channels.get("539927972270833695").join();
 });
 
 
-client.on('message', message => {//for dev
-  var argresult = message.content.split(` `).slice(1).join(' ');
-    if (!devs.includes(message.author.id)) return;
-
-if (message.content.startsWith(adminprefix + 'setgame')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
+client.on('message', message => {//Toxic Codes
+    var argresult = message.content.split(` `).slice(1).join(' ');//Toxic Codes
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith('^setg')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+     if (message.content === ("^leave")) {//Toxic Codes
+    message.guild.leave();   //Toxic Codes
+  } else  
+  if (message.content.startsWith('^setw')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});//Toxic Codes
+      message.channel.send(`**✅   ${argresult}**`)//Toxic Codes
+  } else 
+  if (message.content.startsWith('^setl')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅   ${argresult}**`)//Toxic Codes
+  } else 
+  if (message.content.startsWith('^sets')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/zero");
+      message.channel.send(`**✅**`)//Toxic Codes
+  }
+  if (message.content.startsWith('^setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
 } else
-  if (message.content.startsWith(adminprefix + 'setname')) {
-client.user.setUsername(argresult).then
-    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
-return message.reply("**من فضلك يجب عليك الانتظآر لمدة ساعتين ومن ثم قم بتغيير الأسم..!**");
+  if (message.content.startsWith('^setprefix')) {//Toxic Codes
+  client.user.setPrefix(argresult).then
+      message.channel.send(`Changing Prefix ..**${argresult}** `)//Toxic Codes
 } else
-  if (message.content.startsWith(adminprefix + 'setavatar')) {
-client.user.setAvatar(argresult);
-  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
-      } else
-if (message.content.startsWith(adminprefix + 'setT')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/faresgameryt");
-    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
+if (message.content.startsWith('^setavatar')) {//Toxic Codes
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);//Toxic Codes
 }
-
-
-    });
+});//Toxic Codes
 
 
 
@@ -194,10 +207,10 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === '^roles') {
-        var roles = message.guild.roles.map(roles => `${roles.name}  |  `).join(' ')
+        var roles = message.guild.roles.map(roles => `${roles.name} :military_medal: `).join(' ')
         const embed = new Discord.RichEmbed()
         .setColor('#ff0000')
-        .addField('Roles:',`**[${roles}]**`)
+        .addField('**Roles:**',`**[${roles}]**`)
         message.channel.sendEmbed(embed);
     }
 });
